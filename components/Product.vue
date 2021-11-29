985d9b36063555c2ac2e7288edadf110956d89b1
DESKTOP-GI2JMHE\kondrashkin.dev <mikhail.kondrashkin.00@gmail.com>
2021-11-30 00:19:34 +0300

    Инициализировал проект, поменял дефолтный layout, добавил scss в конфигурацию, создал assets, в него добавил глобальные стили и картинку продукта, изменил конфиг stylelint, создал компонент продукта и сверстал его, пока что захардкодив текст

---
+++ components/Product.vue
@@ -0,0 +1,44 @@
+<template>
+  <article class="product">
+    <img src="~assets/images/Polaroid.jpg" alt="Polaroid" />
+    <h2 class="product__title">Наименование товара</h2>
+    <p class="product__text">
+      Довольно-таки интересное описание товара в несколько строк. Довольно-таки
+      интересное описание товара в несколько строк
+    </p>
+    <div class="product__price">10 000 руб.</div>
+  </article>
+</template>
+
+<style lang="scss" scoped>
+.product {
+  padding: 1rem;
+  background: #fffefb;
+  border-radius: rem(4);
+  box-shadow: 0 rem(20) rem(30) rgba(0, 0, 0, 0.04),
+    0 rem(6) rem(10) rgba(0, 0, 0, 0.02);
+}
+.product__title {
+  color: #3f3f3f;
+  font-weight: 600;
+  font-size: rem(20);
+  line-height: em(25);
+  &:not(:last-child) {
+    margin-bottom: 1em;
+  }
+}
+.product__text {
+  color: #3f3f3f;
+  font-size: 1rem;
+  line-height: em(20);
+  &:not(:last-child) {
+    margin-bottom: 2em;
+  }
+}
+.product__price {
+  color: #3f3f3f;
+  font-weight: 600;
+  font-size: rem(24);
+  line-height: em(30);
+}
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,5 @@
+<template>
+  <main class="app">
+    <Nuxt />
+  </main>
+</template>
