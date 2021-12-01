52aaedb62ae63efdde2ec46c8db142ec2383b430
DESKTOP-GI2JMHE\kondrashkin.dev <mikhail.kondrashkin.00@gmail.com>
2021-12-01 18:50:33 +0300

    Поменял форму добавления товара,  id создаваемого продукта добавляю с помощью lodash, в шаблоне использую кастомное поле ввода

--- components/AddProduct.vue
+++ components/AddProduct.vue
@@ -1,37 +1,41 @@
 <template>
   <div class="add-product">
     <form class="add-product__form" @submit.prevent="addProduct">
-      <label>
-        Наименование товара
-        <input
-          v-model="product.title"
-          type="text"
-          placeholder="Введите наименование товара"
-        />
-      </label>
-      <label>
-        Описание товара
-        <textarea placeholder="Введите описание товара" />
-      </label>
-      <label>
-        Ссылка на изображение товара
-        <input type="text" placeholder="Ссылка на изображение товара" />
-      </label>
-      <label>
-        Цена товара
-        <input type="text" placeholder="Введите цену" />
-      </label>
-      <button type="submit">Добавить товар</button>
+      <TextField
+        v-model="product.title"
+        label="Наименование товара"
+        placeholder="Введите наименование товара"
+      />
+      <TextField
+        v-model="product.description"
+        label="Описание товара"
+        placeholder="Введите описание товара"
+        textarea
+      />
+      <TextField
+        v-model="product.image"
+        label="Ссылка на изображение товара"
+        placeholder="Введите ссылку"
+      />
+      <TextField
+        v-model="product.price"
+        label="Цена товара"
+        placeholder="Введите цену"
+      />
+      <button class="add-product__form-submit" type="submit">
+        Добавить товар
+      </button>
     </form>
   </div>
 </template>
+
 <script>
+const uniqueId = require('lodash.uniqueid')
 export default {
   data() {
     return {
       product: {
-        id: new Date(),
-        title: '',
+        id: uniqueId(),
       },
     }
   },
@@ -46,8 +50,40 @@ export default {
 <style lang="scss" scoped>
 .add-product {
   flex: 1 0 332px;
+  padding: rem(24);
   background: #fffefb;
   border-radius: 4px;
   box-shadow: 0 20px 30px rgba(0, 0, 0, 0.04), 0 6px 10px rgba(0, 0, 0, 0.02);
+  @media (min-width: em(768)) {
+    &:not(:last-child) {
+      margin-right: 1rem;
+    }
+  }
+  @media (max-width: em(768)) {
+    &:not(:last-child) {
+      margin-bottom: 1rem;
+    }
+  }
+}
+.add-product__form {
+  display: flex;
+  flex-direction: column;
+}
+
+.add-product__form-submit {
+  padding: em(11) 0;
+  color: #fff;
+  font-weight: 600;
+  font-size: 1rem;
+  font-family: 'Inter', sans-serif;
+  line-height: em(15);
+  background: #7bae73;
+  border: none;
+  border-radius: 10px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  cursor: pointer;
+  @media (min-width: em(768)) {
+    font-size: rem(12);
+  }
 }
 </style>
